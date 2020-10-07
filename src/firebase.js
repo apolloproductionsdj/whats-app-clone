4dd73654a148ad6f9ed7d4b2d76ad6865be7ac3a
Mike Torres <apolloproductionsdj@gmail.com>
2020-10-07 10:05:49 -0700

    Initial Commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB08-CS7R2DxfCV_FtR4JN_nlbcM-2RnWk",
+    authDomain: "whatsapp-253a8.firebaseapp.com",
+    databaseURL: "https://whatsapp-253a8.firebaseio.com",
+    projectId: "whatsapp-253a8",
+    storageBucket: "whatsapp-253a8.appspot.com",
+    messagingSenderId: "756173763813",
+    appId: "1:756173763813:web:e5972f503670350257900c",
+    measurementId: "G-XWZKVKTWQD"
+};
